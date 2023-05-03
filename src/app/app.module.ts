495d787c0a2c5a8de303ba5cfb4b2c88dc3ce588
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-05-03 09:07:51 +0200

    added ng material - login - register - welcome components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {  MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 import { AppComponent } from './app.component';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { AppRoutingModule } from './app-routing.module';
+import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeScreenComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatMenuModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/welcome-screen/welcome-screen.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+
+@Component({
+  selector: 'app-welcome-screen',
+  templateUrl: './welcome-screen.component.html',
+  styleUrls: ['./welcome-screen.component.scss']
+})
+export class WelcomeScreenComponent implements OnInit {
+  
+    constructor(public dialog: MatDialog) { }
+  
+    ngOnInit(): void {
+    }
+
+    openLogin() {
+      this.dialog.open(LoginComponent);
+    }
+  
+    openSignUp() {
+      this.dialog.open(RegisterComponent);
+    }
+}

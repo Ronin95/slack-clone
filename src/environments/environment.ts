485280e9f107641eeac7ea143c8a4d4ae3b37b40
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-05-09 11:45:31 +0200

    test gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,35 +5,36 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 export const environment = {
-// 	  firebase: {
-//     projectId: 'slack-clone-da',
-//     appId: '1:135875603663:web:9e5addc58e4248cbd21bba',
-//     storageBucket: 'slack-clone-da.appspot.com',
-//     apiKey: 'AIzaSyCRgSI4GSvVWDTFsob_Q0juWjE5aTpbn1k',
-//     authDomain: 'slack-clone-da.firebaseapp.com',
-//     messagingSenderId: '135875603663',
-//   },
-// 	production: false,
-// 	firebaseConfig: {
-// 	  apiKey: "AIzaSyCRgSI4GSvVWDTFsob_Q0juWjE5aTpbn1k",
-// 	  authDomain: "slack-clone-da.firebaseapp.com",
-// 	  projectId: "slack-clone-da",
-// 	  storageBucket: "slack-clone-da.appspot.com",
-// 	  messagingSenderId: "135875603663",
-// 	  appId: "1:135875603663:web:9e5addc58e4248cbd21bba"
-// 	},
+	// 	  firebase: {
+	//     projectId: 'slack-clone-da',
+	//     appId: '1:135875603663:web:9e5addc58e4248cbd21bba',
+	//     storageBucket: 'slack-clone-da.appspot.com',
+	//     apiKey: 'AIzaSyCRgSI4GSvVWDTFsob_Q0juWjE5aTpbn1k',
+	//     authDomain: 'slack-clone-da.firebaseapp.com',
+	//     messagingSenderId: '135875603663',
+	//   },
+	// 	production: false,
+	// 	firebaseConfig: {
+	// 	  apiKey: "AIzaSyCRgSI4GSvVWDTFsob_Q0juWjE5aTpbn1k",
+	// 	  authDomain: "slack-clone-da.firebaseapp.com",
+	// 	  projectId: "slack-clone-da",
+	// 	  storageBucket: "slack-clone-da.appspot.com",
+	// 	  messagingSenderId: "135875603663",
+	// 	  appId: "1:135875603663:web:9e5addc58e4248cbd21bba"
+	// 	},
 
-// 	firebase: {
-// 		apiKey: 'AIzaSyDI2YOSnhk2ZCsEH9DEmjKoGzMqS249k8U',
-// 		authDomain: 'slack-clone-2566e.firebaseapp.com',
-// 		projectId: 'slack-clone-2566e',
-// 		storageBucket: 'slack-clone-2566e.appspot.com',
-// 		messagingSenderId: '878899644746',
-// 		appId: '1:878899644746:web:399c1e7ee05ce6fc1259b3',
-// 	},
-// };
+	firebase: {
+		apiKey: 'AIzaSyDI2YOSnhk2ZCsEH9DEmjKoGzMqS249k8U',
+		authDomain: 'slack-clone-2566e.firebaseapp.com',
+		projectId: 'slack-clone-2566e',
+		storageBucket: 'slack-clone-2566e.appspot.com',
+		messagingSenderId: '878899644746',
+		appId: '1:878899644746:web:399c1e7ee05ce6fc1259b3',
+	},
+	openAIToken: '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',
+};
 
- firebase: {
+/* firebase: {
  	apiKey: "AIzaSyDqdOauEP72jzBlIehzTWfbbQYAxaKWkg0",
  	authDomain: "slack-clone-9a73f.firebaseapp.com",
  	projectId: "slack-clone-9a73f",
@@ -42,5 +43,5 @@ export const environment = {
  	appId: "1:88584794093:web:850243c15d4616575581e4"
    },
 
-  openAIToken: '' // inside the '' add your personal api key
-};
+  openAIToken: '' // inside the '' add your personal api key */
+//};

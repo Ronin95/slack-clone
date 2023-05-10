9542b684f9944ce89ba5369657f21bc594a142d6
ShawnKastner <shawn.kastner@icloud.com>
2023-05-10 12:17:17 +0200

    auto complete at send msg, show all user emails and remove old register function from register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,39 +6,22 @@ import {
   Validators,
 } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { LoginComponent } from '../login/login.component';
-import { User } from 'src/models/user.class';
 import {
   Firestore,
-  collection,
-  doc,
-  setDoc,
-  getDocs,
 } from '@angular/fire/firestore';
 import { AuthService } from '../services/auth.service';
 import { VerifyEmailComponent } from '../verify-email/verify-email.component';
-
-// import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
   firestore: Firestore = inject(Firestore);
-
-  user: User = new User();
-  allUsers: any[] = [];
-
-  async ngOnInit() {
-    // await this.getUsers();
-    // console.table(this.allUsers);
-  }
 
   public registerForm: FormGroup;
 
-  // , public authService: AuthService
   constructor(
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<RegisterComponent>,
@@ -58,28 +41,4 @@ export class RegisterComponent implements OnInit {
       this.dialog.open(VerifyEmailComponent);
     }
   }
-
-  /**
-   * Registers a new user
-   */
-  // registerUser() {
-  // this.user.toJSON();
-  // console.log(this.user);
-  // const aCollection = collection(this.firestore, 'users'); //? collection() creates a reference to the collection 'users' in the database 'firestore
-  // const aDoc = doc(aCollection); //? Creates a reference to a document in a collection
-  // setDoc(aDoc, this.user.toJSON()); //? setDoc() sets the data to the document
-  // }
-
-  /**
-   * Gets all users from the database
-   */
-  async getUsers() {
-    const querySnapshot = await getDocs(collection(this.firestore, 'users'));
-    querySnapshot.forEach((doc) => {
-      this.allUsers.push(doc.data());
-    });
-  }
-
-  //hier weiter
-  doubleEntry() {}
 }

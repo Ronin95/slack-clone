073fc39b3c7fa5d2b7a1503f38fb18294bdec315
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-05-10 14:15:40 +0200

    quill continued

--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import Quill from 'quill';
 import BlotFormatter from 'quill-blot-formatter';
 Quill.register('modules/blotFormatter', BlotFormatter);
+import 'quill-emoji/dist/quill-emoji.js';
 
 @Component({
 	selector: 'app-text-editor',
@@ -12,6 +13,13 @@ Quill.register('modules/blotFormatter', BlotFormatter);
 export class TextEditorComponent implements OnInit {
 	templateForm: FormGroup;
 	quillEditorModules = {};
+
+	modules = {};
+	content = '';
+
+	blured = false;
+	focused = false;
+
 	ngOnInit(): void {}
 
 	constructor() {
@@ -20,6 +28,34 @@ export class TextEditorComponent implements OnInit {
 		});
 		this.quillEditorModules = {
 			blotFormatter: {},
+		};
+
+		this.modules = {
+			'emoji-shortname': true,
+			'emoji-textarea': true,
+			'emoji-toolbar': true,
+			toolbar: [
+				['bold', 'italic', 'underline', 'strike'], // toggled buttons
+				['blockquote', 'code-block'],
+
+				[{ header: 1 }, { header: 2 }], // custom button values
+				[{ list: 'ordered' }, { list: 'bullet' }],
+				[{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+				[{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+				[{ direction: 'rtl' }], // text direction
+
+				[{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+				[{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+				[{ color: [] }, { background: [] }], // dropdown with defaults from theme
+				[{ font: [] }],
+				[{ align: [] }],
+
+				['clean'], // remove formatting button
+
+				['link', 'image', 'video'], // link and image, video
+				['emoji'],
+			],
 		};
 	}
 

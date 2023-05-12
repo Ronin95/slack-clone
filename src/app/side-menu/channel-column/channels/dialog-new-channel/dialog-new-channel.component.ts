947fa81b57805adbdff52a34f35f20a5239b22e7
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-05-12 12:13:01 +0200

    new attribute to channelCollection, channelChat

--- src/app/side-menu/channel-column/channels/dialog-new-channel/dialog-new-channel.component.ts
+++ src/app/side-menu/channel-column/channels/dialog-new-channel/dialog-new-channel.component.ts
@@ -3,29 +3,31 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { Firestore, collection, doc, setDoc } from '@angular/fire/firestore';
 
 @Component({
-  selector: 'app-dialog-new-channel',
-  templateUrl: './dialog-new-channel.component.html',
-  styleUrls: ['./dialog-new-channel.component.scss'],
+	selector: 'app-dialog-new-channel',
+	templateUrl: './dialog-new-channel.component.html',
+	styleUrls: ['./dialog-new-channel.component.scss'],
 })
 export class DialogNewChannelComponent {
-  firestore: Firestore = inject(Firestore);
+	firestore: Firestore = inject(Firestore);
 
-  channelName: string = '';
+	channelName: string = '';
+	channelChat: object[] = [];
 
-  constructor(public dialogRef: MatDialogRef<DialogNewChannelComponent>) {}
+	constructor(public dialogRef: MatDialogRef<DialogNewChannelComponent>) {}
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+	onNoClick(): void {
+		this.dialogRef.close();
+	}
 
-  addChannel() {
-    const channelCollection = collection(this.firestore, 'channels');
-    setDoc(doc(channelCollection), { channelName: this.channelName })
-    .then(() => {
-      console.log('New channel added:', this.channelName);
-      
-        this.dialogRef.close();
-      }
-    );
-  }
+	addChannel() {
+		const channelCollection = collection(this.firestore, 'channels');
+		setDoc(doc(channelCollection), {
+			channelName: this.channelName,
+			channelChat: this.channelChat,
+		}).then(() => {
+			console.log('New channel added:', this.channelName);
+
+			this.dialogRef.close();
+		});
+	}
 }

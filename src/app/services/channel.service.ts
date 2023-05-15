e0215076d17e66cf575644e18b362bf1bc064089
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-05-15 16:25:40 +0200

    accessing channels from services

--- src/app/services/channel.service.ts
+++ src/app/services/channel.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, OnInit, inject } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import {
 	Firestore,
@@ -13,25 +13,27 @@ import { Observable } from 'rxjs';
 	providedIn: 'root',
 })
 export class ChannelService {
-	firestore: Firestore = inject(Firestore);
-	channels$!: Observable<any>;
-	channel!: Array<any>;
-	channelId!: any;
+  firestore: Firestore = inject(Firestore);
+  channels$!: Observable<any>;
+  channel!: Array<any>;
+  channelId!: any;
 
-	constructor(private afs: AngularFirestore) {}
+  constructor(private afs: AngularFirestore) {}
 
-	getAllChannels() {
-		const channelColl = collection(this.firestore, 'channels');
-		this.channels$ = collectionData(channelColl, { idField: 'channelId' });
-		this.channels$.subscribe((changes: any) => {
-			this.channel = changes;
-			console.log(this.channel);
-		});
-	}
+  getAllChannels(): Observable<any> {  // Return type is now Observable<any>
+    const channelColl = collection(this.firestore, 'channels');
+    this.channels$ = collectionData(channelColl, { idField: 'channelId' });
+    this.channels$.subscribe((changes: any) => {
+      this.channel = changes;
+      console.log(this.channel);
+    });
+    return this.channels$;  // Return the Observable
+  }
 
-	deleteChannel(channelId: any) {
-		const channelColl = collection(this.firestore, 'channels');
-		const docRef = doc(channelColl, channelId);
-		deleteDoc(docRef);
-	}
+  deleteChannel(channelId: any) {
+    const channelColl = collection(this.firestore, 'channels');
+    const docRef = doc(channelColl, channelId);
+    deleteDoc(docRef);
+  }
 }
+
--- src/app/side-menu/channels/channels.component.ts
+++ src/app/side-menu/channels/channels.component.ts
@@ -1,13 +1,7 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogNewChannelComponent } from './dialog-new-channel/dialog-new-channel.component';
-import {
-	Firestore,
-	collection,
-	collectionData,
-	deleteDoc,
-	doc,
-} from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { ChannelService } from 'src/app/services/channel.service';
 
@@ -17,18 +11,23 @@ import { ChannelService } from 'src/app/services/channel.service';
 	styleUrls: ['./channels.component.scss'],
 })
 export class ChannelsComponent implements OnInit {
+  @Input()
+  channel!: {
+    channelId: string;
+    channelName: string;
+  }[];
 	firestore: Firestore = inject(Firestore);
 	channels$!: Observable<any>;
-	channel!: Array<any>;
 	channelId!: any;
 	openMenu: boolean = false;
 
 	constructor(public dialog: MatDialog, private channelService: ChannelService) {
-		/* 	this.channelService.getAllChannels(); */
 	}
 
 	ngOnInit() {
-		this.channelService.getAllChannels();
+		this.channelService.getAllChannels().subscribe((channels: any) => {
+      this.channel = channels;
+    });
 	}
 
 	toggleChannels() {
@@ -39,16 +38,7 @@ export class ChannelsComponent implements OnInit {
 		this.dialog.open(DialogNewChannelComponent);
 	}
 
-	/* 	getAllChannels() {
-		const channelColl = collection(this.firestore, 'channels');
-		this.channels$ = collectionData(channelColl, { idField: 'channelId' });
-		this.channels$.subscribe((changes: any) => {
-			this.channel = changes;
-			console.log(this.channel);
-		});
-	} */
-
 	onDeleteChannel(channelId: any) {
-		this.channelService.deleteChannel(this.channelId);
+		this.channelService.deleteChannel(channelId);
 	}
 }

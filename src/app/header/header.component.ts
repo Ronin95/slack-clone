e43b772733713f07871cca17fb6936376bfa9189
ShawnKastner <shawn.kastner@icloud.com>
2023-06-28 12:16:36 +0200

    added a toggle button to side menu and set the height for name date style at private chats

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogUserInfoComponent } from './dialog-user-info/dialog-user-info.component';
-import {
-	AngularFirestore,
-	AngularFirestoreDocument,
-} from '@angular/fire/compat/firestore';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Observable, Subject, filter, debounceTime, of } from 'rxjs';
-import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { ChannelService } from '../services/channel.service';
-import { UsersService } from '../services/users.service';
 
 @Component({
 	selector: 'app-header',
@@ -20,11 +12,8 @@ import { UsersService } from '../services/users.service';
 export class HeaderComponent implements OnInit {
 	constructor(
 		public dialog: MatDialog,
-		private afs: AngularFirestore,
 		public authService: AuthService,
-		private afAuth: AngularFireAuth,
 		public channelService: ChannelService,
-		private usersService: UsersService
 	) {
 	}
 

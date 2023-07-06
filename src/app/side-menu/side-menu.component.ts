a4d9225ac0753833ed2b013585da1a11e497bb3e
ShawnKastner <shawn.kastner@icloud.com>
2023-07-06 12:54:43 +0200

    side menu responsive

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss'],
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  @ViewChild('drawer')
+  drawer!: MatDrawer;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngAfterViewInit() {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe((result) => {
+        if (result.matches) {
+          this.drawer.mode = 'over';
+          this.drawer.close();
+        } else {
+          this.drawer.mode = 'side';
+          this.drawer.open();
+        }
+      });
+  }
+}
